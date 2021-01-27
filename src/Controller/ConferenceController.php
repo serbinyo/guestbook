cbedc4d7d1dba09111da8240199dbcfa1fbe8c87
Александр Сербин <serbin@rarus.ru>
2021-01-27 10:31:44 +0300

    Добавлены сущности Conference и Comment их репозитории и применены миграции

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +12,25 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
+//        return $this->render('conference/index.html.twig', [
+//            'controller_name' => 'ConferenceController',
+//        ]);
+
+        return new Response(<<<EOF
+<html>
+    <body>
+    $greet
+        <img src="/images/under-construction.gif" />
+    </body>
+    </html>
+EOF
+        );
     }
 }
