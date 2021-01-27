ba508da3d19ac3b3890376ca78611ab2512a1451
Александр Сербин <serbin@rarus.ru>
2021-01-27 11:38:47 +0300

    Добавлен шаблонизатор Twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,35 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     * @param Environment          $twig
+     * @param ConferenceRepository $conferenceRepository
+     *
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function index(Request $request): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        $greet = '';
-        if ($name = $request->query->get('hello')) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-
-//        return $this->render('conference/index.html.twig', [
-//            'controller_name' => 'ConferenceController',
-//        ]);
-
-        return new Response(<<<EOF
-<html>
-    <body>
-    $greet
-        <img src="/images/under-construction.gif" />
-    </body>
-    </html>
-EOF
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 }
